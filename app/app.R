ad2f89bd3878591c5f229a0819cebd94a8f23e7b
martinmontane <martinmontane@gmail.com>
2019-12-03 20:18:34 -0300

    Corrección de bugs y nueva funcionalidad para leer los datos desde JUNAR

--- app/app.R
+++ app/app.R
@@ -7,50 +7,6 @@
                  'data.table','gsubfn','dqshiny','shinyjs','httpuv')
   lapply(librerias, require, character.only=TRUE)
   
-  #################################################
-  ## Data wrangling que ya no es necesario hacer ##
-  ## hay que modificarlo cuando se unifique todo ##
-  ## en una sola planilla en junar               ##
-  #################################################
-  
-  #   datosSalud <- fread("http://vicentelopez.cloudapi.junar.com/api/v2/datastreams/LISTA-DE-ENTID-22632/data.csv/?auth_key=1cbc9a823e07baae05ab006066a47d4906d8adb9",encoding = 'UTF-8', header=FALSE) %>%
-  #                 .[,CAT:='Salud']
-  #   colnames(datosSalud)[1:5] <- c('CATLVL2','Nombre','Direccion','Barrio','ContactoTelefono')
-  # 
-  #   datosDiscapacidad <- fread("http://vicentelopez.cloudapi.junar.com/api/v2/datastreams/LISTA-DE-ENTID-94887/data.csv/?auth_key=1cbc9a823e07baae05ab006066a47d4906d8adb9",encoding = 'UTF-8', header=FALSE) %>%
-  #            .[, CAT:='Discapacidad']
-  #   colnames(datosDiscapacidad)[1:5] <- c('Nombre','Objetivo','Direccion','Barrio','ContactoTelefono')
-  #   datosParticipacionSocial <- fread("http://vicentelopez.cloudapi.junar.com/api/v2/datastreams/LISTA-DE-ENTID/data.csv/?auth_key=1cbc9a823e07baae05ab006066a47d4906d8adb9",encoding = 'UTF-8', header=FALSE) %>%
-  #                               .[,CAT:= 'Participacion Social']
-  #   colnames(datosParticipacionSocial)[1:5] <- c('CATLVL2','Nombre','Direccion','Barrio','ContactoTelefono')
-  #   datosAdultosMayores <- fread("http://vicentelopez.cloudapi.junar.com/api/v2/datastreams/LISTA-DE-ENTID-29685/data.csv/?auth_key=1cbc9a823e07baae05ab006066a47d4906d8adb9",encoding = 'UTF-8', header=FALSE) %>%
-  #                          .[,CAT:= 'Adultos Mayores']
-  #   colnames(datosAdultosMayores)[1:4] <- c('Nombre','Direccion','Barrio','ContactoTelefono')
-  # 
-  #   datosCultoColectividades <- fread("http://vicentelopez.cloudapi.junar.com/api/v2/datastreams/LISTA-DE-ENTID-13756/data.csv/?auth_key=1cbc9a823e07baae05ab006066a47d4906d8adb9",encoding = 'UTF-8', header=FALSE) %>%
-  #                               .[, CAT:='Culto y Colectividades']
-  #   colnames(datosCultoColectividades)[1:6] <- c('CATLVL2','Nombre','Direccion','Barrio','ContactoTelefono','ContactoMail')
-  # 
-  #   datosCulturaEducacion <- fread("http://vicentelopez.cloudapi.junar.com/api/v2/datastreams/LISTA-DE-ENTID-90402/data.csv/?auth_key=1cbc9a823e07baae05ab006066a47d4906d8adb9",encoding = 'UTF-8', header=FALSE) %>%
-  #                            .[,CAT:='Cultura y Educacion']
-  #   colnames(datosCulturaEducacion)[1:5] <- c('CATLVL2','Nombre','Direccion','Barrio','ContactoTelefono')
-  # 
-  #   datosDeportesRecreacion <- fread("http://vicentelopez.cloudapi.junar.com/api/v2/datastreams/LISTA-DE-ENTID-58717/data.csv/?auth_key=1cbc9a823e07baae05ab006066a47d4906d8adb9",encoding = 'UTF-8', header=FALSE) %>%
-  #     .[,CAT:='Deportes y Recreacion']
-  #   colnames(datosDeportesRecreacion)[1:5] <- c('CATLVL2','Nombre','Direccion','Barrio','ContactoTelefono')
-  # 
-  # datos <- rbind(datosAdultosMayores,datosCultoColectividades,datosCulturaEducacion,datosDeportesRecreacion,
-  #                datosDiscapacidad,datosParticipacionSocial,datosSalud, fill=TRUE)
-  # datos$Direccion <- ifelse(datos$Direccion %in% 'Rivadavia 2350', 'Bernardino Rivadavia 2350',datos$Direccion)
-  # 
-  # require(ggmap)
-  # latlong <- geocode(paste0(datos$Direccion,', ',datos$Barrio,
-  #                ", Vicente López, ",
-  #                "Argentina"))
-  # datos$lon <- latlong$lon
-  # datos$lat <- latlong$lat
-  # write.table(datos,'datosGeolocalizados.csv',sep=';',row.names=FALSE)
-  
   # Carga de funciones auxiliares
   source('auxiliares.R')
   
@@ -61,19 +17,24 @@
   # Barrio: unidad geográfica mínima donde se ubica la organización,
   # podría ser barrio o culquier otra medida (por ejemplo, comuna).
   # ContactoTelefono: numero de teléfono para ser contactado
-  # CAT: Categoría según se organizan las instituciones. Esta pensado
+  # Categoria: Categoría según se organizan las instituciones. Esta pensado
   # para que cada una de tenga solo una categoría
   # ContactoMail: mail de contacto
   # Descricipcion: Breve párrafo sobre las actividades que realiza
-  # Lon: longitud de la coordenada donde se ubica (WGS84)
-  # Lat: latitud de la coordenada donde se ubica (WGS84)
+  # lon: longitud de la coordenada donde se ubica (WGS84)
+  # lat: latitud de la coordenada donde se ubica (WGS84)
+  datos <- fread('http://vicentelopezdevo.cloudapi.junar.com/api/v2/datastreams/TEST-ENTID/data.csv/?auth_key=f20f529b8190a209f7d8da7987344ce8a5676c7a',
+                 encoding = 'UTF-8')
+  # Si existen oboservaciones que están vacías, pero son textos, hay que
+  # convertirlo
+  # Data Wrangling específico para contactos
+  datos <- as.data.table(lapply(datos, function(x) ifelse(x=='', NA, x)))
   
-  datos <- fread('datosGeolocalizados.csv',encoding = 'UTF-8')
-  # Definición de íconos.
+ # Definición de íconos.
   # awesomeIconList es una función que nos permite unir a las categorias de las variables
   # con un ícono particular de las librerias de font awesome o ion icons.
   # Por default, los nombres de la lista hacen referencia a las
-  # categorías de la variable CAT
+  # categorías de la variable Categoria
   VLIcons <- awesomeIconList(
     Discapacidad = makeAwesomeIcon(icon = 'fa-wheelchair',library = 'fa',markerColor = 'white',iconColor = 'black'),
     Salud = makeAwesomeIcon(icon = 'ios-medkit',library = 'ion',markerColor = 'white',iconColor = 'black'),
@@ -88,17 +49,9 @@
   # en el sistema de coordenada de referencia WGS84
   datos <- st_as_sf(datos,coords = c('lon','lat'))
   
-  # Si existen oboservaciones que están vacías, pero son textos, hay que
-  # convertirlo a NAs, esto vale tanto para las variables de contacto
-  # como la de descripción u objetivo
-  # Data Wrangling específico para contactos
-  datos$ContactoMail <- ifelse(datos$ContactoMail =='',NA,datos$ContactoMail)
-  datos$ContactoTelefono <- ifelse(datos$ContactoTelefono =='',NA,datos$ContactoTelefono)
-  datos$Objetivo <- ifelse(datos$Objetivo =='',NA,datos$Objetivo)
-  
   # Crea los popups que se van a ver en el mapa.
   
-  # Se basan en una función "createPupups", disponible en el archivo auxiliares.R
+  # Se basan en una función "createPopups", disponible en el archivo auxiliares.R
   # Algunos parámetros, tales como nombre, tematica, contactoTel y contactoMail
   # solo piden el nombre de la columna. Esto es así porque son campos que
   # en general no necesitan formateo especial para aparecer en el popup.
@@ -113,18 +66,18 @@
   
   datos$popup <- createPopups(data = datos,
                               nombre = 'Nombre',
-                              tematica = 'CAT',
+                              tematica = 'Categoria',
                               direccion = paste(datos$Direccion,',',datos$Barrio,',Vicente Lopez', sep=''),
                               contactoTel = "ContactoTelefono",
                               contactoMail = "ContactoMail",
                               addWhatsapp = TRUE,
                               captionWhatsapp = "Lo encontré en el Mapa de Organizaciones Sociales de Vicente López",
-                              descripcion = datos$Objetivo)
+                              descripcion = datos$Descripcion)
   # Las aplicaciones de shiny tienen dos partes. UI define 
   # cómo se verá la página: qué elementos tendrá, qué estilo, entre otros
   # parámetros. 
   ui <- dashboardPage(
-    dashboardHeader(title = "Mapa interactivo de OSC"),
+    dashboardHeader(title = paste0("Mapa interactivo de OSC (",nrow(datos),')')),
     dashboardSidebar(disable=TRUE),
     dashboardBody(
       shinyjs::useShinyjs(),
@@ -154,9 +107,9 @@
                      pickerInput(
                        inputId = "FiltroTematico",
                        label = "Filtrar por temas",
-                       choices = unique(datos$CAT),
+                       choices = unique(datos$Categoria),
                        multiple = TRUE,
-                       selected = unique(datos$CAT),
+                       selected = unique(datos$Categoria),
                        options = list(
                          size = 5,
                          `actions-box` = TRUE,
@@ -199,11 +152,11 @@
           setView(lng = -58.5,lat = -34.52,zoom = 13)
       } else {
         leaflet(data = datos[datos$Barrio %in% inputs_change()[[1]] & 
-                               datos$CAT %in% inputs_change()[[2]],]) %>%
+                               datos$Categoria %in% inputs_change()[[2]],]) %>%
           addProviderTiles("CartoDB.Positron",
                            options = providerTileOptions(minZoom = 10, maxZoom = 20)) %>%
           addAwesomeMarkers(clusterOptions = markerClusterOptions(),
-                            popup = ~popup,icon = ~VLIcons[CAT],label = ~Nombre) %>%
+                            popup = ~popup,icon = ~VLIcons[Categoria],label = ~Nombre) %>%
           setMaxBounds( lng1 = -(58.5503+0.1)
                         , lat1 = -(34.55554+0.1)
                         , lng2 = -(58.46877-0.1)
@@ -222,7 +175,7 @@
                    st_geometry() %>% unlist(.))[2]
         popupUnico <- datos[datos$Nombre %in% input$SeleccionEntidad,'popup'] %>% unlist() %>% .[1] %>% unname()
         leafletProxy("map") %>%
-          setView(lng = lng,lat = lat,zoom = 20) %>%
+          setView(lng = lng,lat = lat,zoom = 16) %>%
           addPopups(lng = lng, lat = lat,popup=popupUnico) %>%
           setMaxBounds( lng1 = -(58.5503+0.1)
                         , lat1 = -(34.55554+0.1)
@@ -236,7 +189,7 @@
         leafletProxy("map") %>%
           setView(lng = input$long,
                   lat = input$lat,
-                  zoom = 20)  %>%
+                  zoom = 16)  %>%
           setMaxBounds( lng1 = -(58.5503+0.1)
                         , lat1 = -(34.55554+0.1)
                         , lng2 = -(58.46877-0.1)
